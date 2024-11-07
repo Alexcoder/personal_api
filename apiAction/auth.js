cadad0d36376612492870feca37ffef063778eee
Dino <agboalez@gmail.com>
2024-11-07 09:57:48 +0300

    7 november

--- apiAction/auth.js
+++ apiAction/auth.js
@@ -1,12 +1,35 @@
 import { Auth,  } from "../model/auth.js";
 import bcrypt from "bcryptjs"
+import dotenv from "dotenv"
+import nodemailer from "nodemailer"
+
+dotenv.config()
+
+// Transporter setup for nodemailer
+const transporter = nodemailer.createTransport({
+  service: "Gmail", // or use another email provider's settings
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+
 
 const hash =(password)=> bcrypt.hash(password, 10)
 const compare =(enteredPassword, storedPassword)=> bcrypt.compare(enteredPassword, storedPassword);
 
 export const register = async(req, res) =>{
-  console.log("register", req?.body? "arrived": "")
-
+  console.log("register", req?.body? "arrived": "");
+//
+  const mailOptions = {
+  from: process.env.EMAIL,
+  to: req?.body.email,
+  subject: "Complete your registration",
+  // html: `<p>Click <a href="${registrationLink}">here</a> to complete your registration.</p>`,
+  html: `<p>Click <a href="${"registrationLink"}">here</a> to complete your registration.</p>`,
+};
+//
   const newUser ={
     email     : req?.body.email,
     firstName : req?.body.firstName,
@@ -19,6 +42,9 @@ export const register = async(req, res) =>{
      if(existingUser){
       return res.status(400).json("User Already Exist")
      }
+     await transporter.sendMail(mailOptions);
+    //  res.status(200).json({ message: "Registration email sent!" });
+
      const hashPassword = await hash(req?.body.password)
      const savedUser = await new Auth({...newUser, hashPassword}).save();
      res.status(200).json({
